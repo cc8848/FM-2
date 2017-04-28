595e74094a1fd9db72d06701cf2d77888058c2e6
zxcvbnm <zhong.sw@qq.com>
2017-04-28 12:01:12 +0800

    完成专辑旋转动画和'切换下一首'等功能

---
+++ js/script.js
@@ -0,0 +1,58 @@
+(() => {
+  let albums = [
+    {
+      "name": "Without You",
+      "artist": "Mariah Carey",
+      "song": "Without You"
+    },
+    {
+      "name": "Bury the Hatchet",
+      "artist": "The Cranberries",
+      "song": "Dying In the Sun"
+    },
+     {
+       "name": "HIStory",
+       "artist": "Michael Jackson",
+       "song": "You Are Not Alone"
+     }
+   ];
+  let audio = document.querySelector('audio');
+  let albumCover = document.querySelector('.album-cover');
+  let songName = document.querySelector('.song-name');
+  let artist = document.querySelector('.artist');
+  let stepForward = document.querySelector('.fa-step-forward');
+  let n = 0;
+
+  function loadAlbum(album) {
+   audio.src = `./albums/${album.name}/${album.song}.mp3`;
+   albumCover.style.backgroundImage = `url('./albums/${album.name}/cover.jpg')`;
+   songName.textContent = album.song;
+   document.title = `${album.song} - FM`;
+   artist.textContent = album.artist;
+  }
+
+  function next() {
+    if (n === albums.length - 1) {
+      n = 0;
+    } else {
+      n++;
+    }
+    loadAlbum(albums[n]);
+    albumCover.style.animationPlayState = 'running';
+    audio.play();
+  }
+
+  audio.onended = next;
+  stepForward.onclick = next;
+  albumCover.onclick = function() {
+    if (this.style.animationPlayState === 'running') {
+      audio.pause();
+      this.style.animationPlayState = 'paused';
+    } else {
+      audio.play();
+      this.style.animationPlayState = 'running';
+    }
+  };
+
+  loadAlbum(albums[n]);
+})();
